612035a4767b1098f0404485a758f49a88d1fa0e
graceasuncion <ghasuncion@ucdavis.edu>
2026-04-16 07:16:13 -0700

    added text styling

--- studio1/script.js
+++ studio1/script.js
@@ -3,4 +3,22 @@
 
     console.log("running js");
 
+    const hoverText = document.querySelector("#hover");
+
+    const phrases = [
+    "Lock In Twin",
+    "Keep going.",
+    "It will all be worth it in the end",
+    "Focus up.",
+    "One more try.",
+    "You're doing great."
+    ];
+
+    let index = 0;
+
+    hoverText.addEventListener("mouseenter", function() {
+        index = (index + 1) % phrases.length;
+        hoverText.textContent = phrases[index];
+});
+
 }());
