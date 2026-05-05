ce52d35013c7ecb809f9290d857d0001a57fb9d6
graceasuncion <ghasuncion@ucdavis.edu>
2026-05-05 13:23:15 -0700

    added the html

--- studio3/script.js
+++ studio3/script.js
@@ -1,5 +1,21 @@
 (function(){
  
-
+/*gradient*/
+    const granimInstance = new Granim({
+      element: '#granim-canvas',
+      direction: 'left-right',
+      isPausedWhenNotInView: false,
+      states: {
+        "default-state": {
+          gradients: [
+            ['#004080', '#0077b6', '#023e8a'],
+            ['#0096c7', '#00b4d8', '#0077b6'],
+            ['#023e8a', '#0096c7', '#004080'],
+            ['#0077b6', '#48cae4', '#0096c7'],
+          ],
+          transitionSpeed: 5000,
+        }
+      }
+    });
     
 }());
